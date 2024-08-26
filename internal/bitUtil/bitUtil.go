72b61ab69e3d18433b5242a168ce4d774b4d5346
Oleksii Petrov <a.valorwing@gmail.com>
2024-08-26 20:02:08 +0300

    nano optimization bitutil. write comments bitutil package

--- internal/bitUtil/bitUtil.go
+++ internal/bitUtil/bitUtil.go
@@ -1,5 +1,12 @@
+// Package bitutil provides utilities for reading and writing a specified number
+// of bits to and from a byte slice.
 package bitutil
 
+// WriteBits writes `writeBitLength` bits from `objectToWrite` to `targetArray`
+// starting at the specified `initialByteOffset` and `initialBitOffset` in the target array.
+// If necessary, the function automatically continues writing into the next byte.
+// WARNING: The function does not manage the length of `targetArray`; you must ensure
+// the array has sufficient space for the written bits.
 func WriteBits(objectToWrite byte, writeBitLength uint8, targetArray *[]byte, initialByteOffset uint64, initialBitOffset uint8) {
 	target := *targetArray
 	source := objectToWrite
@@ -14,18 +21,24 @@ func WriteBits(objectToWrite byte, writeBitLength uint8, targetArray *[]byte, in
 		initialBitOffset++
 	}
 }
+
+// ReadBits reads `bitLength` bits from `source`, starting at `byteOffset` and `bitOffset`
+// in the specified byte. The function returns the read byte and a boolean value indicating
+// whether the read operation stayed within the bounds of the source array.
+// If necessary, the function automatically continues reading from the next byte.
 func ReadBits(source *[]byte, bitLength uint8, byteOffset uint64, bitOffset uint8) (byte, bool) {
 	var result byte
-	var sourceTarget = *source
+	sourceTarget := *source
+	sourceLen := uint64(len(*source))
 	for i := uint8(0); i < bitLength; i++ {
 		if bitOffset == 8 {
 			byteOffset++
-			if byteOffset >= uint64(len(*source)) {
+			if byteOffset >= sourceLen {
 				return 0, false
 			}
 			bitOffset = 0
 		}
-		bit := sourceTarget[byteOffset] >> bitOffset & 1
+		bit := (sourceTarget[byteOffset] >> bitOffset) & 1
 		result |= bit << i
 		bitOffset++
 	}
